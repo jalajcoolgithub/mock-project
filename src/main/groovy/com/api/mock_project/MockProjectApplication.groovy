ba5ad36f4ab4c9f9382f7a2a6e5bd021430b23c6
jalajcoolgithub <49112274+jalajcoolgithub@users.noreply.github.com>
2025-08-01 03:32:40 +0530

    first commit

---
+++ src/main/groovy/com/api/mock_project/MockProjectApplication.groovy
@@ -0,0 +1,13 @@
+package com.api.mock_project
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MockProjectApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MockProjectApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/api/mock_project/MockProjectApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.api.mock_project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MockProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
