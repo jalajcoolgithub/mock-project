273cb5826de6116122646c2ee04c0e36a79a3182
jalajcoolgithub <49112274+jalajcoolgithub@users.noreply.github.com>
2025-08-01 09:03:02 +0530

    added youtube link

--- services/project-management/src/main/java/com/api/ProjectManagementApplication.java
+++ services/project-management/src/main/java/com/api/ProjectManagementApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectManagementApplication {
 
+    // https://www.youtube.com/watch?v=QQ4oyr93B8k&t=234s
+
     public static void main(String[] args) {
         SpringApplication.run(ProjectManagementApplication.class, args);
     }
